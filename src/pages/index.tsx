12afa1feb18dfd4574c4684dfddd94afe62ec4ea
guccho6w9 <gusta.quiroga033@gmail.com>
2024-08-09 17:11:32 -0300

    Add(-): index updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-// pages/index.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import '@/app/globals.css';
 import Header from '@/components/header';
 import InfoLocal from '@/components/infolocal';
@@ -8,6 +7,12 @@ import ProductList from '@/components/productList';
 import products from '@/data/products';
 
 const Home: React.FC = () => {
+  const [openCategory, setOpenCategory] = useState<string | null>(null);
+
+  const handleCategoryClick = (category: string) => {
+    setOpenCategory((prevCategory) => (prevCategory === category ? null : category));
+  };
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="container mx-auto lg:max-w-screen-lg">
@@ -19,11 +24,16 @@ const Home: React.FC = () => {
       <div className="container mx-auto lg:max-w-screen-lg flex-grow lg:border-l lg:border-r lg:border-gray-300 lg:px-4">
         <main className="p-4 flex-grow">
           {Object.keys(products).map((category, index) => (
-            <ProductList key={index} category={category} products={products[category]} />
+            <ProductList
+              key={index}
+              category={category}
+              products={products[category]}
+              isOpen={openCategory === category}
+              onClick={() => handleCategoryClick(category)}
+            />
           ))}
         </main>
       </div>
-
     </div>
   );
 };
