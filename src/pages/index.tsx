b774155b3277787da01b4a8d4e59958851667e16
guccho6w9 <gusta.quiroga033@gmail.com>
2024-08-15 09:47:01 -0300

    Add(-): updated index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import '@/app/globals.css';
 import Header from '@/components/header';
 import InfoLocal from '@/components/infolocal';
+import CartButton from '@/components/cartButton';
 import Footer from '@/components/footer';
 import ProductList from '@/components/productList';
 import products from '@/data/products';
+import { CartProvider } from '@/context/cartContext';
 
 const Home: React.FC = () => {
   const [openCategory, setOpenCategory] = useState<string | null>(null);
@@ -14,27 +16,30 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="container mx-auto lg:max-w-screen-lg">
-        <Header />
+    <CartProvider>
+      <div className="min-h-screen flex flex-col">
+        <div className="container mx-auto lg:max-w-screen-lg">
+          <Header />
+        </div>
+        <div className="container mx-auto lg:max-w-screen-lg">
+          <InfoLocal />
+        </div>
+        <div className="container mx-auto lg:max-w-screen-lg flex-grow lg:border-l lg:border-r lg:border-gray-300 lg:px-4">
+          <main className="p-4 flex-grow">
+            {Object.keys(products).map((category, index) => (
+              <ProductList
+                key={index}
+                category={category}
+                products={products[category]}
+                isOpen={openCategory === category}
+                onClick={() => handleCategoryClick(category)}
+              />
+            ))}
+          </main>
+        </div>
       </div>
-      <div className="container mx-auto lg:max-w-screen-lg">
-        <InfoLocal />
-      </div>
-      <div className="container mx-auto lg:max-w-screen-lg flex-grow lg:border-l lg:border-r lg:border-gray-300 lg:px-4">
-        <main className="p-4 flex-grow">
-          {Object.keys(products).map((category, index) => (
-            <ProductList
-              key={index}
-              category={category}
-              products={products[category]}
-              isOpen={openCategory === category}
-              onClick={() => handleCategoryClick(category)}
-            />
-          ))}
-        </main>
-      </div>
-    </div>
+      <CartButton  />
+    </CartProvider>
   );
 };
 
